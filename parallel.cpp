77b593a4364f73fab6408ea6434dc7c6fc597353
MrDoritos <ianmoore322@gmail.com>
2024-08-28 16:10:16 -0700

    Parallel

---
+++ parallel.cpp
@@ -0,0 +1,9 @@
+//https://stackoverflow.com/questions/54525473/can-someone-help-me-parallelize-this-c-code
+
+/*
+1) take a positive integer N as an argument
+2) create an integer array of size N
+3) populate the integers from range [1,1000]
+4) Find the largest integer and the sum of the array in parallel
+5) print the largest integer and the sum of the array. 
+*/
